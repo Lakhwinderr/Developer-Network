9071e71e2d3a75d5705db0a6b0c8d20623de4c1a
Lakhwinderr <lakhwinder.sidhuu20@gmail.com>
2023-07-29 21:25:54 +0530

    added react project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Widget } from '@typeform/embed-react'
+
+
+const Typeform = () => {
+  return <Widget id="H5vg8GDc" style={{ width: '100%', height: "90vh" }} className="my-form" />
 }
 
-export default App;
+//fetching the data from the responses api
+
+
+
+export default function App() {
+
+  useEffect(() => {
+ const apiToken = 'tfp_2umtj7Ji517VXmQsURXkruez8gKn7B9pSWZSGJwc5awC_3stYNKLUrws81k';
+const formId = "H5vg8GDc"; // Replace with your Typeform form ID
+
+// Endpoint URL to fetch responses from the Typeform API
+const apiUrl = `https://api.typeform.com/forms/${formId}/responses`;
+
+// Fetch Typeform data
+fetch(apiUrl, {
+  method: 'GET',
+  headers: {
+    'Authorization': `Bearer ${apiToken}`,
+  },
+})
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then((data) => {
+    // Process the data received from Typeform API
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error('Error fetching Typeform data:', error);
+  });
+
+}) 
+
+  return (
+    <div>
+      <h1>Creating typeform app</h1>
+      <Typeform></Typeform>
+      
+    </div>
+  )
+}
